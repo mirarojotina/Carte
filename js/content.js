026e624c57f73da9cdc1762b84e5cec61c35fdc7
mirarojotina <mirarojotina@gmail.com>
2020-11-26 22:48:25 +0300

    projet carte de visite

---
+++ js/content.js
@@ -0,0 +1,17 @@
+ var fac = "Institut Supérieur Polytechnique de Madagascar"
+     course ="Informatique de Gestion, Génie Logiciel et Intelligence Artificielle";
+     name = "SAHONDRARIMANANA Mira Rojotina";
+     spinneret = "IGGLIA 5 n° 25";
+     adress = "Lot AA 74 B Ampahimanga Ambohimanambola";
+     phone = "0324938759";
+     mail ="mirarojotina@gmail.com";
+  document.getElementById("fac").innerText = fac;
+  document.getElementById("course").innerText = course;
+  document.getElementById("name").innerText = name;
+  document.getElementById("spinneret").innerText = spinneret;
+  document.getElementById("adress").innerText = adress;
+  document.getElementById("phone").innerText = phone;
+  document.getElementById("mail").innerText = mail;
+
+ 
+
